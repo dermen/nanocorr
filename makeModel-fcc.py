42f6c0d77c1251fc9d2d7597a265ab2025e23e35
dermen <dermen@stanford.edu>
2013-04-24 13:29:30 -0700

    bye bye numpy

--- makeModel-fcc.py
+++ makeModel-fcc.py
@@ -1,20 +1,19 @@
-from sys import argv
-import numpy as np
+import math
 
 #a           =  4.090 #silver lattice parameter
 
 a           =  4.0786 # the real lattice parameter for gold!!
 atomID      =  79
-atom0       = np.array( [  0.,    0.,    0.] )
-atom1       = np.array( [  0.,  a/2.,  a/2.] )
-atom2       = np.array( [a/2.,    0.,  a/2.] )
-atom3       = np.array( [a/2.,  a/2.,    0.] )
-atoms       = np.vstack(  ( atom0, atom1, atom2, atom3 )  )
-atom_per_unit   = atoms.shape[0]
+atom0       =  (    0.,    0.,    0.) 
+atom1       =  (    0.,  a/2.,  a/2.) 
+atom2       =  (  a/2.,    0.,  a/2.) 
+atom3       =  (  a/2.,  a/2.,    0.) 
+atoms       =  ( atom0, atom1, atom2, atom3 )
 
-#size_in_nm  = float ( input("size in nm: " ) ) 
+atom_per_unit   = len( atoms )
 
-size_in_nm  = float ( argv[1] ) 
+size_in_nm  = float ( input("size in nm: " ) ) 
+
 size_in_ang = size_in_nm * 10.
 
 center      = ( size_in_ang / 2., size_in_ang / 2., size_in_ang / 2. )
@@ -33,14 +32,11 @@ while u1 < size_in_ang:
                 atomX = u1 + atoms[i][0]
                 atomY = u2 + atoms[i][1]
                 atomZ = u3 + atoms[i][2]
-                radius = np.sqrt( ( atomX - center[0] )**2 + \
+                radius = math.sqrt( ( atomX - center[0] )**2 + \
                                   ( atomY - center[1] )**2 + \
                                   ( atomZ - center[2] )**2  ) 
                 if radius <= size_in_ang / 2:
-                    #coors =  " ".join(  [str(atomX), str(atomY), str(atomZ), str(atomID) ] )
                     print >> out_file , atomX,atomY,atomZ,atomID
-                    #np.savetxt( out_file, modelSphere, \
-                     #       delimiter= " ", fmt='%.3f'  )
                 i += 1
             u3 += a
         u2 += a
